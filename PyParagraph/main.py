302a0612aeb139536c3685960f12065f2637f26d
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2019-11-25 02:49:21 -0600

    Initial Commit

---
+++ PyParagraph/main.py
@@ -0,0 +1,7 @@
+# Python Homework - PyParagraph
+# Submitted by : Sheetal Bongale
+# Python 3.7.4
+# This Python script will automate the analysis of any passage using a set of metrics
+
+import os
+import csv
