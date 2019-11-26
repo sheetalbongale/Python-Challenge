a8f853bd3e67b58ec05ce36329fee56404b27d5f
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2019-11-25 21:05:59 -0600

    Print statement format and variable declaration

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -5,3 +5,16 @@
 
 import os
 import csv
+
+word_count = 0
+sentence_count = 0
+letter_count = 0
+sentence_length = 0
+
+dash_break = "-----------------------------"
+print(
+    "Paragraph Analysis \n" + dash_break + "\n"
+    f"Approximate Word Count: {word_count} \n" + 
+    f"Approximate Sentence Count: {sentence_count} \n" +
+    f"Average Letter Count: {letter_count} \n" +
+    f"Average Sentence Length: {sentence_length} \n" + dash_break)
