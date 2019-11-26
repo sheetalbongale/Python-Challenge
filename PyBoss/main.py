81fccda1c51aa90bfa3f58af5a9a1ca5f37c690a
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2019-11-25 18:22:13 -0600

    Append the new header list with converted data

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -6,63 +6,40 @@
 import os
 import csv
 
+# Declaration of empty header lists for parsed data
+emp_id = []
+first_name = []
+last_name = []
+dob =[]
+ssn = []
+state = []
+
+# Source: https://gist.github.com/afhaque/29f0f4f37463c447770517a6c17d08f5
 us_state_abbrev = {
-    'Alabama': 'AL',
-    'Alaska': 'AK',
-    'Arizona': 'AZ',
-    'Arkansas': 'AR',
-    'California': 'CA',
-    'Colorado': 'CO',
-    'Connecticut': 'CT',
-    'Delaware': 'DE',
-    'Florida': 'FL',
-    'Georgia': 'GA',
-    'Hawaii': 'HI',
-    'Idaho': 'ID',
-    'Illinois': 'IL',
-    'Indiana': 'IN',
-    'Iowa': 'IA',
-    'Kansas': 'KS',
-    'Kentucky': 'KY',
-    'Louisiana': 'LA',
-    'Maine': 'ME',
-    'Maryland': 'MD',
-    'Massachusetts': 'MA',
-    'Michigan': 'MI',
-    'Minnesota': 'MN',
-    'Mississippi': 'MS',
-    'Missouri': 'MO',
-    'Montana': 'MT',
-    'Nebraska': 'NE',
-    'Nevada': 'NV',
-    'New Hampshire': 'NH',
-    'New Jersey': 'NJ',
-    'New Mexico': 'NM',
-    'New York': 'NY',
-    'North Carolina': 'NC',
-    'North Dakota': 'ND',
-    'Ohio': 'OH',
-    'Oklahoma': 'OK',
-    'Oregon': 'OR',
-    'Pennsylvania': 'PA',
-    'Rhode Island': 'RI',
-    'South Carolina': 'SC',
-    'South Dakota': 'SD',
-    'Tennessee': 'TN',
-    'Texas': 'TX',
-    'Utah': 'UT',
-    'Vermont': 'VT',
-    'Virginia': 'VA',
-    'Washington': 'WA',
-    'West Virginia': 'WV',
-    'Wisconsin': 'WI',
-    'Wyoming': 'WY',
+    'Alabama': 'AL','Alaska': 'AK','Arizona': 'AZ','Arkansas': 'AR','California': 'CA','Colorado': 'CO','Connecticut': 'CT','Delaware': 'DE','Florida': 'FL',
+    'Georgia': 'GA','Hawaii': 'HI','Idaho': 'ID','Illinois': 'IL','Indiana': 'IN','Iowa': 'IA','Kansas': 'KS','Kentucky': 'KY','Louisiana': 'LA',
+    'Maine': 'ME','Maryland': 'MD','Massachusetts': 'MA','Michigan': 'MI','Minnesota': 'MN','Mississippi': 'MS','Missouri': 'MO','Montana': 'MT',
+    'Nebraska': 'NE','Nevada': 'NV','New Hampshire': 'NH','New Jersey': 'NJ','New Mexico': 'NM','New York': 'NY','North Carolina': 'NC','North Dakota': 'ND',
+    'Ohio': 'OH','Oklahoma': 'OK','Oregon': 'OR','Pennsylvania': 'PA','Rhode Island': 'RI','South Carolina': 'SC','South Dakota': 'SD',
+    'Tennessee': 'TN','Texas': 'TX','Utah': 'UT','Vermont': 'VT','Virginia': 'VA','Washington': 'WA','West Virginia': 'WV','Wisconsin': 'WI','Wyoming': 'WY',
 }
 
 # Open and read employee_data.csv file:
-with open(os.path.join('Resources','employee_data.csv'),'r') as in_file:
-    employee_data = csv.reader(in_file)
-    header = next(employee_data)
-print(header)
+with open(os.path.join('Resources','employee_data.csv'),'r') as old_file:
+    old_employee_data = csv.DictReader(old_file)
+    # Append the new header lists after converting the old header to the new format:
+    for row in old_employee_data:
+        
+        emp_id.append(row['Emp ID'])
+        first_name.append(row['Name'].split(" ")[0])
+        last_name.append(row['Name'].split(" ")[1])
+        dob.append(row['DOB'].split('-')[1] + '/' + row['DOB'].split('-')[2] + '/' + row['DOB'].split('-')[0])
+        ssn.append('***-**-' + row['SSN'].split('-')[2])
+        state.append(us_state_abbrev[row['State']])
 
+    
 # Create the required header format in a new csv file
+#with open('new_employee_data' , 'w+') as new_file:
+    #new_header = ["Emp ID","First Name","Last Name","DOB","SSN","State"]
+    #write
+
