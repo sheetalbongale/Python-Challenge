0a0d4d5df00c6d467e43aa34208c33037d4bbbb5
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2019-11-25 21:58:54 -0600

    Export and Print output text file

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -5,16 +5,27 @@
 
 import os
 import csv
+import re
 
 word_count = 0
 sentence_count = 0
 letter_count = 0
 sentence_length = 0
 
+
+
 dash_break = "-----------------------------"
-print(
+print_lines = {
     "Paragraph Analysis \n" + dash_break + "\n"
     f"Approximate Word Count: {word_count} \n" + 
     f"Approximate Sentence Count: {sentence_count} \n" +
     f"Average Letter Count: {letter_count} \n" +
-    f"Average Sentence Length: {sentence_length} \n" + dash_break)
+    f"Average Sentence Length: {sentence_length} \n" + dash_break
+}
+
+# Print & Export the Paragraph analysis text file:
+analysis_file = open('Paragraph_analysis.txt','w+')
+analysis_file.writelines(print_lines)
+analysis_file.close()
+analysis_file = open('Paragraph_analysis.txt','r+')
+print(analysis_file.read())
